a0d083801895208bf29d09797da4d5bd7e5ecc26
Jonathan <olivjonathan.s@gmail.com>
2026-09-09 20:25:13 -0300

    Add device classification and server counter

--- hostnames.py
+++ hostnames.py
@@ -1,10 +1,21 @@
 # Filtra hostnames de servidores a partir de uma lista de inventario
 
-hostnames = ["SRV-FILE-01", "SRV-DC-02", "WKS-RH-115"]
+hostnames = ["SRV-FILE-01", "SRV-DC-02", "WKS-RH-115", "SW-CORE-01", "PRN-ADM-03"]
 
-for hostname in hostnames: 
+contador = 0
+
+for hostname in hostnames:
     partes = hostname.split("-")
     tipo = partes[0]
     numero = partes[-1]
     if tipo == "SRV":
-        print(tipo, numero)
+        print("Servidor:", numero)
+        contador = contador + 1    
+    elif tipo == "WKS":
+        print("Workstation:", numero)
+    elif tipo == "SW":
+        print("Switch:", numero)
+    else:
+        print("Desconhecido:", tipo, numero)
+
+print("Total de Servidores:", contador)
